26c845c443df09fd869307bdb1ca85f177378199
Damian Palka <damianadrian.palka@skola.taby.se>
2025-10-23 13:01:21 +0000

    Del 2: Slumpat tal och gissning

--- Gissatalet.py
+++ Gissatalet.py
@@ -1,4 +1,19 @@
+import random as rand
+
 namn = input("Vad heter du?")
 print ("")
 
 print (f"Välkommen till spelet {namn}!")
+print ("")
+
+rätt_tal = rand.randint(1,11)
+print (f"{rätt_tal}")
+
+gissning = int(input("Gissa ett tal mellan 1 och 10:"))
+print ("")
+
+if gissning == rätt_tal:
+    print ("Du har gissat rätt")
+else:
+    print ("Du har gissat fel")
+    print (f"Rätta svaret var {rätt_tal}")
