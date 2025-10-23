4f29b1cfe2d75a3bb0798ad1b4d3831fb76203ed
Damian Palka <damianadrian.palka@skola.taby.se>
2025-10-23 13:08:43 +0000

    Del 2: Slumpat tal och gissning REDO

--- Gissatalet.py
+++ Gissatalet.py
@@ -7,12 +7,10 @@ print (f"Välkommen till spelet {namn}!")
 print ("")
 
 rätt_tal = rand.randint(1,11)
-print (f"{rätt_tal}")
 
 gissning = int(input("Gissa ett tal mellan 1 och 10:"))
 print ("")
 
-print ("Försök 1")
 if gissning == rätt_tal:
     print ("Du har gissat rätt")
 else:
